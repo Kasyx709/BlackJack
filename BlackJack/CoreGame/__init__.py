0198de2d063df6b52c57ca1222d75664b41c308e
Aegis <34630356+Aegis@users.noreply.github.com>
2019-05-24 00:28:50 -0500

    Building Player Client

--- BlackJack/CoreGame/__init__.py
+++ BlackJack/CoreGame/__init__.py
@@ -13,10 +13,13 @@ class House(BaseManager):
 
 
 class Player(object):
-    def __init__(self, name=None):
-        self.name = name
-        self.points = 0
 
+    def __init__(self, address=None, name=None):
+        self.address = ("localhost", 8000) if not address else (r"{}".format(address), 8000)
+        self.pname = name
+        super(Player, self).__init__()
+        self.client = Client(address=self.address, authkey=self.pname)
+        self.points = 0
 
     def player_turn(self):
         pass
@@ -30,3 +33,7 @@ class Dealer(object):
     def dealer_turn(self):
         if self.points > 16:
             pass
+
+
+player = Player(name='Mr Taco')
+print(player.name)
